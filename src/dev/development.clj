1c810282a08c8afc027ffcd3fd1cea78313a6f63
Michael Whitford <michael@whitford.us>
2025-11-16 11:43:47 -0700

    cleanup

--- src/dev/development.clj
+++ src/dev/development.clj
@@ -1,16 +1,18 @@
 (ns development
   (:require
-    [clojure.tools.namespace.repl :as tools-ns :refer [set-refresh-dirs]]
-    [com.fulcrologic.rad.ids :refer [new-uuid]]
-    [com.fulcrologic.rad.type-support.date-time :as dt]
-    [datomic.client.api :as d]
-    [mount.core :as mount]
-    [taoensso.timbre :as log]
-    [us.whitford.facade.components.database :refer [datomic-connections]]
-    [us.whitford.facade.components.ring-middleware]
-    [us.whitford.facade.components.server]
-    [us.whitford.facade.components.statecharts :refer [statecharts]]
-    [us.whitford.facade.model.account :refer [new-account]]))
+   [clojure.tools.namespace.repl :as tools-ns :refer [set-refresh-dirs]]
+   [com.fulcrologic.rad.ids :refer [new-uuid]]
+   [com.fulcrologic.rad.type-support.date-time :as dt]
+   [datomic.client.api :as d]
+   [mount.core :as mount]
+   [taoensso.timbre :as log]
+   [us.whitford.facade.components.database :refer [datomic-connections]]
+   [us.whitford.facade.components.ring-middleware]
+   [us.whitford.facade.components.server]
+   [us.whitford.facade.components.swapi :refer [swapi-martian]]
+   [us.whitford.facade.components.hpapi :refer [hpapi-martian]]
+   [us.whitford.facade.components.statecharts :refer [statecharts]]
+   [us.whitford.facade.model.account :refer [new-account]]))
 
 ;; Prevent tools-ns from findinga source in other places, such as resources
 (set-refresh-dirs "src/main" "src/dev")
@@ -18,15 +20,16 @@
 (comment
   (let [db (d/db (:main datomic-connections))
         env (:env statecharts)]
-    (d/pull db '[*] [:account/id (new-uuid 001)])))
+    (d/pull db '[*] [:account/id (new-uuid 001)]))
+  (:server-url swapi-martian)
+  (:server-url hpapi-martian))
 
 (defn seed! []
   (dt/set-timezone! "America/Phoenix")
   (let [connection (:main datomic-connections)]
     (when connection
-          (log/info "SEEDING data.")
-          (d/transact connection {:tx-data [(new-account "michael@whitford.us")]})
-          )))
+      (log/info "SEEDING data.")
+      (d/transact connection {:tx-data [(new-account "michael@whitford.us")]}))))
 
 (defn start []
   (mount/start-with-args {:config "config/dev.edn"})
